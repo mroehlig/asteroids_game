ce9fb08bc26b03f8fa6c31d9aedff149ca84517d
Martin Röhlig <martin.roehlig@uni-rostock.de>
2024-03-29 13:24:50 +0100

    Updated base url in vite config.

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig, splitVendorChunkPlugin } from "vite";
 
 export default defineConfig({
+  base: "/asteroids_game/",
   server: { host: "0.0.0.0", port: 8080 },
   clearScreen: false,
   plugins: [splitVendorChunkPlugin()],
